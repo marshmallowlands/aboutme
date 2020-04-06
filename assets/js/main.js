f152ca68e90f2bcc2550c2b036391e1fed0fadea
marshmallowlands <2145136703@qq.com>
2020-04-06 09:01:16 +0800

    update site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,123 +1,200 @@
-/*
-	Stellar by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+jQuery(window).on('load', function() {
+        
+    // HIDE PRELAODER
+    $(".preloader").addClass("preloader-hidden");
 
-(function($) {
+    // SHOW/ANIMATE ANIMATION CONTAINER
+    setTimeout(function(){
 
-	var	$window = $(window),
-		$body = $('body'),
-		$main = $('#main');
+        $(".hero .animation-container").each(function(){
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+            var e = $(this);
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+            setTimeout(function(){
 
-	// Nav.
-		var $nav = $('#nav');
+                e.addClass("run-animation");
 
-		if ($nav.length > 0) {
+            }, e.data("animation-delay") );
 
-			// Shrink effect.
-				$main
-					.scrollex({
-						mode: 'top',
-						enter: function() {
-							$nav.addClass('alt');
-						},
-						leave: function() {
-							$nav.removeClass('alt');
-						},
-					});
+        });
 
-			// Links.
-				var $nav_a = $nav.find('a');
+    }, 900 );
 
-				$nav_a
-					.scrolly({
-						speed: 1000,
-						offset: function() { return $nav.height(); }
-					})
-					.on('click', function() {
+});
 
-						var $this = $(this);
 
-						// External link? Bail.
-							if ($this.attr('href').charAt(0) != '#')
-								return;
+jQuery(document).ready(function($) {
+	"use strict";
+    
+    
+    // INIT PARALLAX PLUGIN
+    $(".hero .background-content.parallax-on").parallax({
+        scalarX: 24,
+        scalarY: 15,
+        frictionX: 0.1,
+        frictionY: 0.1,
+    });
+    
+    
+    // SCROLL TOP BUTTON
+    $(".scroll-top").click(function() {
+        
+      $("html, body").animate({ scrollTop: 0 }, 400);
+      return false;
+        
+    });
+    
+    
+    // SCROLL REVEAL SETUP
+    window.sr = ScrollReveal();
+    sr.reveal(".scroll-animated-from-bottom", { 
+        duration: 600,
+        delay: 500,
+        origin: "bottom",
+        rotate: { x: 0, y: 0, z: 0 },
+        opacity: 0,
+        distance: "20vh",
+        viewFactor: 0.4,
+        scale: 1,
+        useDelay: 'onload',
+    });
+    
+    
+    // IMAGE CAROUSEL
+    $('.image-carousel').owlCarousel({
+        center: true,
+        items: 1,
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsive:{
+            800:{
+                items: 2,
+            },
+        }
+    });
+    
+    
+    // HERO/BUTTON ON SCROLL ANIMATING
+    function onScrollAnimating() {
+		
+		var windowHeight = $( ".hero" ).height(),
+            frontContent = $(".hero .front-content"),
+            backContent = $(".hero .background-content"),
+            navigationButton = $( ".navigation-button" ),
+			scrollOffset,
+			calculatedOpacityFrontContent,
+			calculatedScaleFrontContent,
+			calculatedTranslateHeader,
+			calculatedOpacityBackground;
+        
+		
+		function navigationButtonHide() {
+			
+			if ( calculatedTranslateHeader <= 200 ) {
+                
+			    navigationButton.css( "transform", "translateX(" + calculatedTranslateHeader + "%) translateY(-50%)");
 
-						// Deactivate all links.
-							$nav_a
-								.removeClass('active')
-								.removeClass('active-locked');
-
-						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-							$this
-								.addClass('active')
-								.addClass('active-locked');
-
-					})
-					.each(function() {
-
-						var	$this = $(this),
-							id = $this.attr('href'),
-							$section = $(id);
-
-						// No section for this link? Bail.
-							if ($section.length < 1)
-								return;
-
-						// Scrollex.
-							$section.scrollex({
-								mode: 'middle',
-								initialize: function() {
-
-									// Deactivate section.
-										if (browser.canUse('transition'))
-											$section.addClass('inactive');
-
-								},
-								enter: function() {
-
-									// Activate section.
-										$section.removeClass('inactive');
-
-									// No locked links? Deactivate all links and activate this section's one.
-										if ($nav_a.filter('.active-locked').length == 0) {
-
-											$nav_a.removeClass('active');
-											$this.addClass('active');
-
-										}
-
-									// Otherwise, if this section's link is the one that's locked, unlock it.
-										else if ($this.hasClass('active-locked'))
-											$this.removeClass('active-locked');
-
-								}
-							});
-
-					});
-
+			} else if ( scrollOffset > windowHeight ) {
+                
+			    navigationButton.css( "transform", "translateX(200%) translateY(-50%)");
+				
+			}
 		}
+		
+		function frontContentMargin() {
+			
+			if ( scrollOffset <= windowHeight ) {
 
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000
+				frontContent.css( "margin-top", scrollOffset );
+
+			} else if ( scrollOffset > windowHeight ) {
+				
+				frontContent.css( "margin-top", windowHeight );
+				
+			}
+			
+		}
+		
+		function frontContentOpacity() {
+			
+			if ( calculatedOpacityFrontContent >= 0 ) {
+
+				frontContent.css( "opacity", calculatedOpacityFrontContent);
+
+			} else if ( scrollOffset > windowHeight ) {
+				
+				frontContent.css( "opacity", "0");
+				
+			}
+		}
+		
+		function frontContentScale() {
+			
+			if ( calculatedScaleFrontContent >= 0.4 ) {
+				
+				frontContent.css( "transform", "scale(" + calculatedScaleFrontContent + ")");
+
+			} else if ( scrollOffset > windowHeight ) {
+				
+				frontContent.css( "transform", "scale(0.6)");
+				
+			}
+			
+		}
+		
+		function backgroundOpacity() {
+			
+			if ( calculatedOpacityBackground >= 0 ) {
+
+				backContent.css( "opacity", calculatedOpacityBackground );
+
+			} else if ( scrollOffset > windowHeight ) {
+				
+				backContent.css( "opacity", "0" );
+				
+			}
+			
+		}
+		
+		function runStep() {
+			
+			scrollOffset = $( window ).scrollTop();
+            
+            if(windowHeight > scrollOffset && scrollOffset >= 0) {
+			
+                calculatedTranslateHeader = ( scrollOffset / windowHeight) * 650;
+                calculatedOpacityFrontContent = 1 - ( scrollOffset / windowHeight) * 4.2;
+                calculatedScaleFrontContent = 1 - ( scrollOffset / windowHeight) * 1.2;
+                calculatedOpacityBackground = 1 - ( scrollOffset / windowHeight) * 1.4;
+
+                navigationButtonHide();
+                frontContentMargin();
+                frontContentOpacity();
+                frontContentScale();
+                backgroundOpacity();
+                
+            }
+			
+		}
+		
+		$( window ).on( 'resize', function(){
+
+			windowHeight = $( ".hero" ).height();
+
 		});
 
-})(jQuery);
+		$( window ).scroll(function() {
+            
+			runStep();
+
+		});
+		
+		runStep();
+		
+	}
+	
+	onScrollAnimating();
+    
+    
+});
